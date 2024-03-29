643173aa06a3d5b56d6c33c6bff6e3a42ddfe907
Georgij Gičan <georgij.gican@nortal.com>
2024-03-29 23:30:15 +0200

    pass

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 import { ListComponent } from './list/list.component';
-import { PurchasesListComponent } from './purchases/purchases-list.component';
-import { PurchaseLinesListComponent } from './purchases/purchase-lines-list.component';
+import { PurchasesListComponent } from './purchases/purchase-list/purchases-list.component';
+import { PurchaseLinesListComponent } from './purchases/purchase-lines/purchase-lines-list.component';
 
 
 const routeConfig: Routes = [
